6a3fd2bc27a41171cbce04e6f1d5e1e392e4e5d2
yuanzhihai <396751927@qq.com>
2022-10-29 13:34:28 +0800

    修复 qiniu

--- src/driver/Qiniu.php
+++ src/driver/Qiniu.php
@@ -4,7 +4,7 @@ declare( strict_types = 1 );
 namespace yzh52521\filesystem\driver;
 
 
-use Overtrue\Flysystem\QiNiu\QiniuAdapter;
+use Overtrue\Flysystem\Qiniu\QiniuAdapter;
 use yzh52521\filesystem\Driver;
 
 class Qiniu extends Driver
@@ -13,8 +13,8 @@ class Qiniu extends Driver
     protected function createAdapter()
     {
         return new QiniuAdapter(
-            $this->config['access_key'], $this->config['secret_key'],
-            $this->config['bucket'], $this->config['domain']
+            $this->config['accessKey'], $this->config['secretKey'],
+            $this->config['bucket'], $this->config['url']
         );
     }
 }
