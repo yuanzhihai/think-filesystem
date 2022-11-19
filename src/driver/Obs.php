d32024b4c69428ab26b8371db7783ad4443906dd
yuanzhihai <396751927@qq.com>
2022-11-19 21:07:37 +0800

    update obsadpter

--- src/driver/Obs.php
+++ src/driver/Obs.php
@@ -16,16 +16,16 @@ class Obs extends Driver
 
     protected function createAdapter()
     {
-        $config                      = [];
+        $config                      = $this->config;
         $root                        = $this->config['root'] ?? '';
         $options                     = $this->config['options'] ?? [];
         $portableVisibilityConverter = new PortableVisibilityConverter(
             $this->config['directory_visibility'] ?? $this->config['visibility'] ?? Visibility::PUBLIC
         );
-        $config['bucket_endpoint']   ??= $this->config['is_cname'] ?? false;
-        $config['token']             ??= $this->config['security_token'] ?? null;
-        $config['is_cname']          = $this->config['bucket_endpoint'];
-        $config['security_token']    = $this->config['token'];
+        $config['is_cname']          ??= $this->config['is_cname'] ?? false;
+        $config['token']             ??= $this->config['token'] ?? null;
+        $config['bucket_endpoint']   = $this->config['bucket_endpoint'];
+        $config['security_token']    = $this->config['security_token'];
         $options                     = array_merge( $options,Arr::only( $config,['url','temporary_url','endpoint','bucket_endpoint'] ) );
         $obsClient                   = new ObsClient( $config );
         return new ObsAdapter( $obsClient,$config['bucket'],$root,$portableVisibilityConverter,null,$options );
