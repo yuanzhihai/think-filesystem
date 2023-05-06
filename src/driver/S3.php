85e33558b39c72b191997c702128893c2c7390c6
yuanzhihai <396751927@qq.com>
2023-05-06 16:30:37 +0800

    update

--- src/driver/S3.php
+++ src/driver/S3.php
@@ -6,6 +6,7 @@ namespace yzh52521\filesystem\driver;
 
 use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 use League\Flysystem\Visibility;
+use think\helper\Arr;
 use yzh52521\filesystem\Driver;
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3V3\PortableVisibilityConverter as AwsS3PortableVisibilityConverter;
@@ -15,12 +16,24 @@ class S3 extends Driver
 
     protected function createAdapter()
     {
-        $client = new S3Client( $this->config );
-        $root = (string) ($this->config['root'] ?? '');
+        $s3Config = $this->formatS3Config($this->config);
+        $root = (string) ($s3Config['root'] ?? '');
         $visibility = new AwsS3PortableVisibilityConverter(
-            $config['visibility'] ?? Visibility::PUBLIC
+            $this->config['visibility'] ?? Visibility::PUBLIC
         );
-        $streamReads = $this->config['stream_reads'] ?? false;
-        return new AwsS3V3Adapter( $client,$this->config['bucket'],$root, $visibility, null, $config['options'] ?? [], $streamReads );
+        $streamReads = $s3Config['stream_reads'] ?? false;
+        $client = new S3Client( $s3Config );
+        return new AwsS3V3Adapter( $client,$s3Config['bucket'],$root, $visibility, null, $this->config['options'] ?? [], $streamReads );
+    }
+
+    protected function formatS3Config(array $config)
+    {
+        $config += ['version' => 'latest'];
+
+        if (! empty($config['key']) && ! empty($config['secret'])) {
+            $config['credentials'] = Arr::only($config, ['key', 'secret', 'token']);
+        }
+
+        return Arr::except($config, ['token']);
     }
 }
